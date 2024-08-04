76569da28d8d1f3f9c7f87b6e8406aa857102451
Christian Farmer <christian.gt.farmer@gmail.com>
2024-08-04 15:17:58 -0500

    Deserialize DB secret

--- src/user-service/src/main/java/com/cgtfarmer/demo/config/DataSourceConfig.java
+++ src/user-service/src/main/java/com/cgtfarmer/demo/config/DataSourceConfig.java
@@ -1,6 +1,11 @@
 package com.cgtfarmer.demo.config;
 
 import com.cgtfarmer.demo.dto.DbSecret;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,21 +18,31 @@ import org.springframework.context.annotation.Profile;
 @Profile({ "deployed" })
 public class DataSourceConfig {
 
+  private final ObjectMapper objectMapper;
+
   @Autowired
-  public DataSourceConfig() {
+  public DataSourceConfig(ObjectMapper objectMapper) {
+    this.objectMapper = new ObjectMapper();
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    objectMapper.registerModule(new JavaTimeModule());
   }
 
   @Bean
   public DataSource devDataSource(
       @Value("${spring.datasource.url}") final String dataSourceUrl,
       @Value("${spring.datasource.driverclassname}") final String driverClassName,
-      @Value("${db.secret}") final DbSecret dbSecret
-  ) {
+      @Value("${db.secret}") final String dbSecretString
+  ) throws JsonMappingException, JsonProcessingException {
     HikariDataSource dataSource = new HikariDataSource();
 
     dataSource.setJdbcUrl(dataSourceUrl);
 
     dataSource.setDriverClassName(driverClassName);
+
+    DbSecret dbSecret = this.objectMapper.readValue(
+        dbSecretString,
+        DbSecret.class
+    );
 
     dataSource.setUsername(dbSecret.getUsername());
 
